706b3438c3d129e563af9efb26a91b304a5a14b7
Brandon Chao <bchao1@stuy.edu>
2019-02-03 23:22:12 -0500

    sqrt with check for 0, with corresponding group class main class

--- recursion.java
+++ recursion.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class recursion{
+public class Recursion{
     /*You may write additional private methods */
 
     /*Recursively find the sqrt using Newton's approximation
@@ -9,15 +9,31 @@ public class recursion{
 
     */
     public static double sqrt(double n, double tolerance){
-
+      double guest = 1;
+      if (n == 0) {
+        return 0;
+      }
+      return sqrts(n,tolerance,guest);
     }
 
+    public static double guesser(double n, double g){
+      return (n / g + g) / 2;
+    }
+
+    public static double sqrts(double n, double tolerance, double guess) {
+      if ((((guess * guess - n) / n) > -tolerance) && (((guess * guess - n) / n) < tolerance)){
+        return guess;
+      }
+      else {
+        return sqrts (n, tolerance, guesser(n,guess));
+      }
+    }
     /*Recursively find the n'th fibbonaci number in linear time
      *fib(0) = 1; fib(1) = 1; fib(5) = 5
      *precondition: n is non-negative
      */
     public static int fib(int n){
-
+      return 0;
     }
 
     /*As Per classwork*/
@@ -35,6 +51,68 @@ public class recursion{
         mS(n-1,sum);
       }
       return sums;
+    }
+    //Courtesy of class group list
+    public static void main(String[] args) {
+            System.out.println("sqrt()");
+            System.out.println("-----------------------\n");
+
+            System.out.println("sqrt(4, 0.00001)");
+            System.out.println("EXPECTED: 2");
+            System.out.println(sqrt(4, 0.00001));         // 2
+
+            System.out.println("----");
+
+            System.out.println("sqrt(8, 0.00001)");
+            System.out.println("EXPECTED: ~2.8284");
+            System.out.println(sqrt(8, 0.00001));         // ~2.8284
+
+            System.out.println("----");
+
+            System.out.println("sqrt(0, 0.00001)");
+            System.out.println("EXPECTED: 0");
+            System.out.println(sqrt(0, 0.00001));         // 0
+
+            System.out.println("\n");
+            /*
+            System.out.println("fib()");
+            System.out.println("-----------------------\n");
+
+            System.out.println("fib(0)");
+            System.out.println("EXPECTED: 0");
+            System.out.println(fib(0));                   // 0
+
+            System.out.println("----");
+
+            System.out.println("fib(1)");
+            System.out.println("EXPECTED: 1");
+            System.out.println(fib(1));                   // 1
+
+            System.out.println("----");
+
+            System.out.println("fib(7)");
+            System.out.println("EXPECTED: 13");
+            System.out.println(fib(7));                   // 13
+
+            System.out.println("makeAllSums()");
+            System.out.println("-----------------------\n");
+
+            System.out.println("makeAllSums(0)");
+            System.out.println("EXPECTED: [0]");
+            System.out.println(makeAllSums(0));           // [0]
+
+            System.out.println("----");
+
+            System.out.println("makeAllSums(1)");
+            System.out.println("EXPECTED: [1, 0]");
+            System.out.println(makeAllSums(1));           // [1, 0]
+
+            System.out.println("----");
+
+            System.out.println("makeAllSums(3)");
+            System.out.println("EXPECTED: [0, 3, 2, 5, 1, 4, 3, 6]");
+            System.out.println(makeAllSums(3));           // [0, 3, 2, 5, 1, 4, 3, 6]
+            */
     }
 
 }
